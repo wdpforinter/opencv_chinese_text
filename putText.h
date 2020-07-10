b15c77a8c5c6da4254baec910b7756ffacf11162
wangdongpo <dongpo.wang@deepcam.com>
2020-07-10 16:03:14 +0800

    update

---
+++ putText.h
@@ -0,0 +1,13 @@
+
+#ifndef PUTTEXT_H_
+#define PUTTEXT_H_
+
+#include <string>
+#include <opencv2/opencv.hpp>
+
+
+//void GetStringSize(HDC hDC, const char* str, int* w, int* h);
+void putTextZH(cv::Mat &dst, const char* str, cv::Point org, cv::Scalar color, int fontSize,
+	const char *fn = "Arial", bool italic = false, bool underline = false);
+
+#endif // PUTTEXT_H_
